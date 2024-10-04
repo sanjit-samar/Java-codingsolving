e191c3b16232dd78231a83c2efb1bf0e26b4117a
sanjit samar <sanjit1525@gmail.com>
2024-10-04 10:50:16 +0530

    rename repo

--- reverseArray.js
+++ reverseArray.js
@@ -1,12 +1,1 @@
 let givenArray = [1,2,3,4,5];
-
-function reverseArray(arg){
-    let newArray = [];
-    for(let i = arg.length -1; i >=0; i--){
-        newArray.push(arg[i]);
-    }
-    return newArray;
-}
-
-let res = reverseArray(givenArray);
-console.log(res);
